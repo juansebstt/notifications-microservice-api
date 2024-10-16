7756390898d735a1b4e48fe3f602b989270b4187
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-15 19:38:20 -0500

    first commit

---
+++ src/main/java/com/notificationsmicroserviceapi/NotificationsMicroserviceApiApplication.java
@@ -0,0 +1,13 @@
+package com.notificationsmicroserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationsMicroserviceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationsMicroserviceApiApplication.class, args);
+    }
+
+}
