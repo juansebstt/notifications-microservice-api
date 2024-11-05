07c038c765566170bcd621d853f805e9cd2e4013
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-11-05 11:44:45 -0500

    Implemented S3Properties

--- src/main/java/com/notificationsmicroserviceapi/common/property/S3Properties.java
+++ src/main/java/com/notificationsmicroserviceapi/common/property/S3Properties.java
@@ -1,4 +1,26 @@
 package com.notificationsmicroserviceapi.common.property;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Component
+@ConfigurationProperties(prefix = "s3")
 public class S3Properties {
+
+    private String accessKey;
+
+    private String secretKey;
+
+    private String bucket;
+    
+    private String zone;
+
 }
