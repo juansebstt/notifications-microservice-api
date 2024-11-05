a90bd96199c90b06d3b4066b5a975c6530b42315
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-11-05 11:38:07 -0500

    Implemented ApiPathConstants

--- src/main/java/com/notificationsmicroserviceapi/util/FileUtilTools.java
+++ src/main/java/com/notificationsmicroserviceapi/util/FileUtilTools.java
@@ -1,4 +1,8 @@
 package com.notificationsmicroserviceapi.util;
 
 public class FileUtilTools {
+
+    public static final String V1_ROUTE = "/v1";
+    public static final String NOTIFICATION_ROUTE = "/notification";
+
 }
