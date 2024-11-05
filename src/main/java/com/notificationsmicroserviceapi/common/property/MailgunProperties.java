80e6e1091110338eadecda5d3532437b52bbfbc9
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-11-05 11:42:36 -0500

    Implemented MailgunProperties

--- src/main/java/com/notificationsmicroserviceapi/common/property/MailgunProperties.java
+++ src/main/java/com/notificationsmicroserviceapi/common/property/MailgunProperties.java
@@ -1,4 +1,21 @@
 package com.notificationsmicroserviceapi.common.property;
 
+import lombok.*;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Component
+@ConfigurationProperties(prefix = "mailgun")
 public class MailgunProperties {
+
+    private String url;
+
+    private String apiKey;
+
 }
