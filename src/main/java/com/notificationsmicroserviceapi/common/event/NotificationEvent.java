6d08fa17027b64c20338b8d032e4a419d54f6ba5
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-11-05 11:40:46 -0500

    Implemented NotificationEvent

--- src/main/java/com/notificationsmicroserviceapi/common/event/NotificationEvent.java
+++ src/main/java/com/notificationsmicroserviceapi/common/event/NotificationEvent.java
@@ -1,4 +1,20 @@
 package com.notificationsmicroserviceapi.common.event;
 
+import jakarta.validation.constraints.Email;
+import lombok.*;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class NotificationEvent {
+
+    @Email
+    private String receiverEmail;
+
+    private String message;
+
 }
