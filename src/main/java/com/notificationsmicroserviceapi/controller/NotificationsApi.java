bb8b0df2abaf21afdd2beb2e7ea847a4d827d385
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-11-07 10:05:12 -0500

    Implemented NotificationsApi

--- src/main/java/com/notificationsmicroserviceapi/controller/NotificationsApi.java
+++ src/main/java/com/notificationsmicroserviceapi/controller/NotificationsApi.java
@@ -1,4 +1,16 @@
 package com.notificationsmicroserviceapi.controller;
 
+import com.notificationsmicroserviceapi.common.constant.ApiPathConstants;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@RequestMapping(ApiPathConstants.V1_ROUTE + ApiPathConstants.NOTIFICATION_ROUTE)
 public interface NotificationsApi {
+
+    @PostMapping("/upload_template")
+    ResponseEntity<String> uploadTemplate(@RequestParam("file") MultipartFile multipartFile);
+
 }
