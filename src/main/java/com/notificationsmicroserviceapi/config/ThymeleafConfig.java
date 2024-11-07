0bcc4760f9c844e9da1ee3e03d5a68c0249516bb
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-11-07 10:01:31 -0500

    Implemented ThymeleafConfig

--- src/main/java/com/notificationsmicroserviceapi/config/ThymeleafConfig.java
+++ src/main/java/com/notificationsmicroserviceapi/config/ThymeleafConfig.java
@@ -1,4 +1,24 @@
 package com.notificationsmicroserviceapi.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templateresolver.StringTemplateResolver;
+
+@Configuration
 public class ThymeleafConfig {
+
+    @Bean
+    public TemplateEngine templateEngine() {
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(stringTemplateResolver());
+        return templateEngine;
+    }
+    @Bean
+    public StringTemplateResolver stringTemplateResolver() {
+        StringTemplateResolver resolver = new StringTemplateResolver();
+        resolver.setTemplateMode("HTML");
+        resolver.setCacheable(false);
+        return resolver;
+    }
 }
