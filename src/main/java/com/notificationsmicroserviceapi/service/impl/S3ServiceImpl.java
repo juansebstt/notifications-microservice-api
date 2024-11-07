c69b94c226508a3d9c44b1c1f733391be99c929c
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-11-07 10:37:18 -0500

    Partially implemented S3ServiceImpl

--- src/main/java/com/notificationsmicroserviceapi/service/impl/S3ServiceImpl.java
+++ src/main/java/com/notificationsmicroserviceapi/service/impl/S3ServiceImpl.java
@@ -1,4 +1,49 @@
 package com.notificationsmicroserviceapi.service.impl;
 
-public class S3ServiceImpl {
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.S3Object;
+import com.notificationsmicroserviceapi.common.property.S3Properties;
+import com.notificationsmicroserviceapi.service.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.Optional;
+
+@Service
+public class S3ServiceImpl implements S3Service {
+
+    private final AmazonS3 amazonS3;
+    private final S3Properties s3Properties;
+
+    @Autowired
+    public S3ServiceImpl(AmazonS3 amazonS3, S3Properties s3Properties) {
+        this.amazonS3 = amazonS3;
+        this.s3Properties = s3Properties;
+    }
+
+    @Override
+    public File getFileByFileName(String fileName) {
+        return Optional.of(fileName)
+                .map(this::getObjectS3)
+                .map(s3Object -> getFileFromInputStream(fileName, s3Object));
+    }
+
+    private File getFileFromInputStream(String fileName, S3Object s3Object) {
+
+        
+
+    }
+
+    private S3Object getObjectS3(String fileKey) {
+
+        return amazonS3.getObject(s3Properties.getBucket(), fileKey);
+
+    }
+
+    @Override
+    public String uploadTemplate(MultipartFile multipartFile) {
+        return "";
+    }
 }
