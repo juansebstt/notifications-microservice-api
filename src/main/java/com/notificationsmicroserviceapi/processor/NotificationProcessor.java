835e998699262e05ca0b4d0327fded5ec613b464
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-11-07 10:22:18 -0500

    Implemented NotificationProcessor

--- src/main/java/com/notificationsmicroserviceapi/processor/NotificationProcessor.java
+++ src/main/java/com/notificationsmicroserviceapi/processor/NotificationProcessor.java
@@ -1,4 +1,30 @@
 package com.notificationsmicroserviceapi.processor;
 
-public class NotificationProcessor {
+import com.notificationsmicroserviceapi.common.event.NotificationEvent;
+import com.notificationsmicroserviceapi.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.function.Function;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class NotificationProcessor implements Function<Flux<NotificationEvent>, Mono<Void>> {
+
+    public final NotificationService notificationService;
+    
+    @Override
+    public Mono<Void> apply(Flux<NotificationEvent> notificationEventFlux) {
+        return notificationEventFlux
+                .doOnNext(entryEvent -> log.info("Recieved event"))
+                .map(notificationService::sendNotification)
+                .doOnNext(notificationEvent -> log.info("Notification Event: {}", notificationEvent))
+                .subscribeOn(Schedulers.parallel())
+                .then();
+    }
 }
