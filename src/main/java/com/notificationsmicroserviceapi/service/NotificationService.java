a3014b32da3eec0b3357f20ac7cc6ba86939fa14
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-11-07 10:09:16 -0500

    Implemented NotificationService

--- src/main/java/com/notificationsmicroserviceapi/service/NotificationService.java
+++ src/main/java/com/notificationsmicroserviceapi/service/NotificationService.java
@@ -1,4 +1,9 @@
 package com.notificationsmicroserviceapi.service;
 
+import com.notificationsmicroserviceapi.common.event.NotificationEvent;
+
 public interface NotificationService {
+
+    String sendNotification(NotificationEvent notificationEvent);
+
 }
